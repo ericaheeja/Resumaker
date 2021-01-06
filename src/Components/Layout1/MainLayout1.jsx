0c395cb23ac3dc66321cce172607fe78ac72e533
Jaewhee Seo <jseo43@my.bcit.ca>
2021-01-05 18:05:02 -0800

    Implement a floating top button
    - A floating top button to scroll to the top of view for mobile version

--- src/Components/Layout1/MainLayout1.jsx
+++ src/Components/Layout1/MainLayout1.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./Home/Home";
 import Portfolio from "./Portfolio/Portfolio";
 import Resume from "./Resume/Resume";
 import About from "./About/About";
 import Skills from "./Skills/Skills";
 
+let initialTopButtonVisible;
+
+const getInitialTopButtonVisible = () => {
+  if (window.innerWidth <= 768) {
+    initialTopButtonVisible = true;
+  } else {
+    initialTopButtonVisible = false;
+  }
+  return initialTopButtonVisible;
+};
+
 export default function MainLayout1() {
+  const [topButtonVisible, setTopButtonVisible] = useState(getInitialTopButtonVisible());
+
+  const resizeScreen = () => {
+    if (window.innerWidth <= 768) {
+      setTopButtonVisible(true);
+    } else {
+      setTopButtonVisible(false);
+    }
+  };
+
+  window.addEventListener("resize", resizeScreen);
+
+  const onClickHandler = () => {
+    console.log("clicked");
+    window.scroll(0, 0);
+  };
+
   return (
     <div className="MainLayout1">
       <Home />
@@ -13,6 +41,13 @@ export default function MainLayout1() {
       <Resume />
       <About />
       <Skills />
+      {topButtonVisible ? (
+        <div class="ui fixed top sticky topButton">
+          <button class="ui icon button" onClick={onClickHandler}>
+            top
+          </button>
+        </div>
+      ) : null}
     </div>
   );
 }
