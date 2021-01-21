64e32b94d6608d558abe9b01860a61d25acc4638
erica <heejaerica@gmail.com>
2021-01-20 16:39:03 -0800

    Edit LoginModal contents and MainPage view

--- src/Components/MainPage/Samples.jsx
+++ src/Components/MainPage/Samples.jsx
@@ -5,7 +5,7 @@ export default function Samples() {
     <div className="Samples">
       <div className="content">
         <h2>See what's possible</h2>
-        <p>
+        <p id="pContent">
           Creatives around the world use Portfolio to showcase their work. See some examples below.
         </p>
       </div>
