536a804fe4b10a1d848a95acce01bb3c84d99804
JceLee <lkm4351@gmail.com>
2021-01-28 23:54:37 +0900

    fixed bugs

--- src/Components/Layout2/Layout2.jsx
+++ src/Components/Layout2/Layout2.jsx
@@ -1,4 +1,4 @@
-import { Layout, Menu } from "antd";
+import {Layout, Menu} from "antd";
 import {
   UploadOutlined,
   UserOutlined,
@@ -8,7 +8,7 @@ import {
   LinkedinOutlined,
   GithubOutlined,
 } from "@ant-design/icons";
-import { Image, Grid } from "semantic-ui-react";
+import {Image, Grid} from "semantic-ui-react";
 import sampleImg from "../../../src/Assets/layout2/sampleFace.jpg";
 import Home from "./Home/Home";
 import About from "./About/About";
@@ -16,12 +16,12 @@ import Work from "./Work/Work";
 import Education from "./Education/Education";
 import Experience from "./Experience/Experience";
 
-const { Header, Content, Footer, Sider } = Layout;
+const {Header, Content, Footer, Sider} = Layout;
 
 export default function Layout2() {
   return (
     <div className="layout2">
-      <Layout style={{ minHeight: "100vh", maxWidth: "1200px", margin: "auto" }}>
+      <Layout style={{minHeight: "100vh", maxWidth: "1200px", margin: "auto"}}>
         <Sider
           breakpoint="lg"
           collapsedWidth="0"
@@ -31,7 +31,7 @@ export default function Layout2() {
               : (document.getElementById("mainContents").style.marginLeft = "300px");
           }}
           width="300px"
-          style={{ background: "#f2f3f7" }}
+          style={{background: "#f2f3f7"}}
         >
           <div className="navbar">
             <Image
@@ -48,36 +48,36 @@ export default function Layout2() {
               centered
               className="img"
             />
-            <div className="siderContents" style={{ textAlign: "center" }}>
+            <div className="siderContents" style={{textAlign: "center"}}>
               <h2>Racheal</h2>
               <h5>
-                <span style={{ color: "#2c98f0" }}>Front-End Developer</span> in Vancouver
+                <span style={{color: "#2c98f0"}}>Front-End Developer</span> in Vancouver
               </h5>
-              <br />
+              <br/>
               <p>HOME</p>
               <p>ABOUT</p>
               <p>{`WORKS & SKILLS`}</p>
               <p>EDUCATION</p>
               <p>EXPERIENCE</p>
               <p>CONTACT</p>
-              <div style={{ fontSize: "30px" }}>
-                <FacebookOutlined style={{ padding: "10px" }} />
-                <InstagramOutlined style={{ padding: "10px" }} />
-                <LinkedinOutlined style={{ padding: "10px" }} />
-                <GithubOutlined style={{ padding: "10px" }} />
+              <div style={{fontSize: "30px"}}>
+                <FacebookOutlined style={{padding: "10px"}}/>
+                <InstagramOutlined style={{padding: "10px"}}/>
+                <LinkedinOutlined style={{padding: "10px"}}/>
+                <GithubOutlined style={{padding: "10px"}}/>
               </div>
             </div>
           </div>
         </Sider>
         <Layout id="mainContents">
           <Content>
-            <Home />
-            <About />
-            <Work />
-            <Education />
-            <Experience />
+            <Home/>
+            <About/>
+            <Work/>
+            <Education/>
+            <Experience/>
           </Content>
-          <Footer style={{ textAlign: "center" }}>Ant Design ©2021 Created by RESUMAKER</Footer>
+          <Footer style={{textAlign: "center"}}>Ant Design ©2021 Created by RESUMAKER</Footer>
         </Layout>
       </Layout>
     </div>
